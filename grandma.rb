7649844db99759abf7e1c887407977973a6ea8fd
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-22 12:36:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma (string)
   if string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elseif string == string.upcase
+  elseif string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
     return "NO, NOT SINCE 1938!"
